39f4f7ab62f10affe975261a6480663e8c997b11
Max Prettyjohns <heshoots9999@gmail.com>
2017-11-02 17:00:12 +0000

    Start of twitter

---
+++ twitter/client.js
@@ -0,0 +1,9 @@
+var Twitter = require('twitter')
+
+client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.BEARER_TOKEN
+})
+
+module.exports = client;
